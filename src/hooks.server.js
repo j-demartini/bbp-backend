8fd606ecd08e4dcb9a0ac5f2d649883881214a96
Joseph DeMartini <josephdemartini@gmail.com>
2025-07-08 19:53:55 -0400

    dwa

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,7 +4,7 @@ export async function handle({ event, resolve }) {
     return new Response(null, {
       status: 204,
       headers: {
-        'Access-Control-Allow-Origin': 'https://bigbeatproductions.com/',
+        'Access-Control-Allow-Origin': 'https://www.bigbeatproductions.com/',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Allow-Credentials': 'true'
@@ -13,7 +13,7 @@ export async function handle({ event, resolve }) {
   }
 
   const response = await resolve(event);
-  response.headers.set('Access-Control-Allow-Origin', 'https://bigbeatproductions.com/');
+  response.headers.set('Access-Control-Allow-Origin', 'https://www.bigbeatproductions.com/');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   response.headers.set('Access-Control-Allow-Credentials', 'true');
