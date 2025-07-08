a7700419c5250f0b469866fa3be87acdb164c243
Joseph DeMartini <josephdemartini@gmail.com>
2025-07-08 19:51:28 -0400

    fixed access control

---
+++ src/hooks.server.js
@@ -0,0 +1,21 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  if (event.request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://bigbeatproductions.com/',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    });
+  }
+
+  const response = await resolve(event);
+  response.headers.set('Access-Control-Allow-Origin', 'https://bigbeatproductions.com/');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  return response;
+}
