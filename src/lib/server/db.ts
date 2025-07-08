c820e7c7d0414cc859e49c45f93c1fe7804602c4
Joseph DeMartini <josephdemartini@gmail.com>
2025-07-08 18:54:34 -0400

    data

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -18,48 +18,30 @@ db.exec(`
 
 // Convert the file to an arrayBuffer
 
-let buffer;
+let buffer = fs.readFileSync(path.join(process.cwd(), 'ArtistData.xlsx'));
 
-console.log(path.join(process.cwd(), 'data', 'ArtistData.xlsx'));
-console.log(path.join(process.cwd(), 'ArtistData.xlsx'));
+console.log("success, continuing");
+const wb = XLSX.read(buffer, { type: 'array' });
 
-if(fs.existsSync(path.join(process.cwd(), 'data', 'ArtistData.xlsx')))
-{
-  buffer = fs.readFileSync(path.join(process.cwd(), 'data', 'ArtistData.xlsx'));
-  console.log("A");
-} else
-if(fs.existsSync(path.join(process.cwd(), 'ArtistData.xlsx')))
-{
-  buffer = fs.readFileSync(path.join(process.cwd(), 'ArtistData.xlsx'));
-  console.log("B");
-}
+// Get the first sheet's data as JSON
+const sheetName = wb.SheetNames[0];
+const ws = wb.Sheets[sheetName];
+const data = XLSX.utils.sheet_to_json(ws);
 
-if(buffer)
-{
+db.exec('DELETE FROM items');
 
-  console.log("success, continuing");
-  const wb = XLSX.read(buffer, { type: 'array' });
+console.log("deleting");
 
-  // Get the first sheet's data as JSON
-  const sheetName = wb.SheetNames[0];
-  const ws = wb.Sheets[sheetName];
-  const data = XLSX.utils.sheet_to_json(ws);
-
-  db.exec('DELETE FROM items');
-
-  console.log("deleting");
-
-  const insert = db.prepare('INSERT INTO items (name, link) VALUES (?, ?)');
-  console.log("inserting rows");
-  const insertMany = db.transaction((rows: { name: string, link?: string }[]) => {
-  for (const row of rows) {
-    if (row.name) {
-      insert.run(row.name, row.link);
-    }
+const insert = db.prepare('INSERT INTO items (name, link) VALUES (?, ?)');
+console.log("inserting rows");
+const insertMany = db.transaction((rows: { name: string, link?: string }[]) => {
+for (const row of rows) {
+  if (row.name) {
+    insert.run(row.name, row.link);
   }
-  });
-  insertMany(data);
 }
+});
+insertMany(data);
 
 export function getArtists(searchTerm : string): { name:string, link:string } [] {
   const stmt = db.prepare('SELECT * FROM items');
